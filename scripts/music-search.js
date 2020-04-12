d185d9ae08851ce7f02cfa89fb25fdcfbaad315d
Griffin Sullivan <gxs331@psu.edu>
2020-04-12 19:04:02 -0400

    added I/O for search bars and Express/Node

--- scripts/music-search.js
+++ scripts/music-search.js
@@ -1,1 +1,21 @@
-console.log ("hello world!")
+var mainSearch = document.getElementById("home-search-input");
+
+if(mainSearch) {
+    mainSearch.addEventListener("keyup", function(e){
+        if(e.keyCode === 13) {
+            let searchInput = mainSearch.value;
+            alert(searchInput);
+        }
+    });
+}
+
+var search = document.getElementById("search-box");
+
+if(search) {
+    search.addEventListener("keyup", function(e){
+        if(e.keyCode === 13) {
+            let searchInput = search.value;
+            alert(searchInput);
+        }
+    });
+}
